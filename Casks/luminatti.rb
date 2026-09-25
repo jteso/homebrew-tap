386ff26f07ae2759eef9472f844664c140f60620
Javier Teso Edilla <javier.tesoedilla@airtrunk.com>
2026-09-25 17:55:16 +1000

    feat: add Luminatti macOS cask

---
+++ Casks/luminatti.rb
@@ -0,0 +1,19 @@
+cask "luminatti" do
+  version "2.32.1"
+
+  on_arm do
+    url "https://github.com/jteso/luminatti/releases/download/v#{version}/Luminatti-macos-arm64.zip"
+    sha256 "46bdd45cb93837e5b408ba92e125b72755360a35d20f6dffea5f191b3b7bb580"
+  end
+
+  on_intel do
+    url "https://github.com/jteso/luminatti/releases/download/v#{version}/Luminatti-macos-x64.zip"
+    sha256 "6539256438d5f9cdb364297b3619cf316f64a63a809dcf51d6faa7c60ea2b305"
+  end
+
+  name "Luminatti"
+  desc "Native macOS code review workspace"
+  homepage "https://github.com/jteso/luminatti"
+
+  app "Luminatti.app"
+end
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# homebrew-tap
+# Homebrew tap
+
+Install the Luminatti macOS app:
+
+```sh
+brew install --cask jteso/tap/luminatti
+```
+
+The existing command line package remains available as a formula:
+
+```sh
+brew install --formula jteso/tap/luminatti
+```
